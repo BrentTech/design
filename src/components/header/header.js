65fe93e36ce26c56e0ee7e8282bea2612e6406ca
Brent Woodward <43894509+BrentTech@users.noreply.github.com>
2019-01-08 23:55:06 -0800

    form scaffolded

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const Header = props => {
   return (
     <header>
       <h1>{props.title}</h1>
       {props.children}
+      <nav>
+        <ul>
+          <li>
+            <a href="/"> Home </a>
+          </li>
+          <li>
+            <a href="/p1"> Page 1 </a>
+          </li>
+          <li>
+            <a href="/p2"> Page 2 </a>
+          </li>
+          <li>
+            <a href="/p2"> Page 3 </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
